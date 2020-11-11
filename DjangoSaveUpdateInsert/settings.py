2648aaa5cc04f71167f484a86bf24b1e98902357
Kensuke Nagae <kyanny@gmail.com>
2020-11-11 22:20:29 +0900

    Add db query logging

--- DjangoSaveUpdateInsert/settings.py
+++ DjangoSaveUpdateInsert/settings.py
@@ -119,3 +119,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
